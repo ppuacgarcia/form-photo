6febbefeb5a4f41b90f462e9322ebc97712c8877
Pablo David Puac García <pablopuacg@gmail.com>
2024-11-03 15:57:56 -0600

    ino code for esp32 web sockets
    now we can use web sockets with esp32 and the express server

---
+++ websockets.ino
@@ -0,0 +1,67 @@
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <WebSocketsClient_Generic.h>
+
+WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+
+#define LED_PIN 4 // LED conectado al pin GPIO 4
+#define USE_SSL true
+
+#if USE_SSL
+  #define WS_SERVER "192.168.0.57"
+  #define WS_PORT 3000
+#else
+  #define WS_SERVER "192.168.0.57" // Dirección IP del servidor
+  #define WS_PORT 3000 // Puerto del servidor WebSocket
+#endif
+
+void webSocketEvent(const WStype_t& type, uint8_t * payload, const size_t& length) {
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.println("[WSc] Disconnected!");
+      break;
+
+    case WStype_CONNECTED:
+      Serial.println("[WSc] Connected to server");
+      webSocket.sendTXT("Connected");
+      break;
+
+    case WStype_TEXT:
+      Serial.printf("[WSc] get text: %s\n", payload);
+      if (strcmp((char*)payload, "encender") == 0) {
+        digitalWrite(LED_PIN, HIGH); // Enciende el LED
+      } else if (strcmp((char*)payload, "apagar") == 0) {
+        digitalWrite(LED_PIN, LOW); // Apaga el LED
+      }
+      break;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+
+  WiFiMulti.addAP("TIGO-AC35", "4D9687413184"); // Ajusta el SSID y contraseña de WiFi
+  while (WiFiMulti.run() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println("Conectado a WiFi");
+
+#if USE_SSL
+  webSocket.beginSSL(WS_SERVER, WS_PORT);
+#else
+  webSocket.begin(WS_SERVER, WS_PORT, "/");
+#endif
+
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(5000);
+  webSocket.enableHeartbeat(15000, 3000, 2);
+}
+
+void loop() {
+  webSocket.loop(); // Mantiene la conexión WebSocket activa
+}
+
+
